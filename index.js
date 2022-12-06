abb924be68decc5e20b59948a480181c85b2d1d7
Groune <98833627+gregoirevaillant@users.noreply.github.com>
2022-12-06 17:50:14 -0600

    last commit

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 let playerScore = 0;
 let computerScore = 0;
+let gameOn = true;
+let playerSelection;
+let computerSelection;
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
-    let computerChoice
-    if (choice == 1)
-        return computerChoice = "ROCK";
-    else if (choice == 2)
-        return computerChoice = "PAPER";
-    else
-        return computerChoice = "SCISSORS";
+    switch (choice) {
+        case 0:
+            return "ROCK";
+        case 1:
+            return "PAPER";
+        case 2:
+            return "SCISSORS";
+    }
 }
 
 function getPlayerChoice() {
@@ -18,29 +22,43 @@ function getPlayerChoice() {
     return choice;
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-
 function playRound(playerSelection, computerSelection) {
     let result;
-    if (playerSelection == computerSelection)
-        result = "It's a draw!";
-    else if (
+    if (playerSelection == computerSelection) {
+        result = "It's a tie!";
+    } else if (
         playerSelection == "ROCK" && computerSelection == "PAPER" ||
         playerSelection == "PAPER" && computerSelection == "SCISSORS" ||
-        playerSelection == "SCISSORS" && computerSelection == "ROCK") {
+        playerSelection == "SCISSORS" && computerSelection == "ROCK"
+    ) {
         result = "You lose!";
         computerScore++;
-    }
-    else if (playerSelection == "SCISSORS" && computerSelection == "PAPER" ||
+    } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER" ||
         playerSelection == "ROCK" && computerSelection == "SCISSORS" ||
-        playerSelection == "PAPER" && computerSelection == "ROCK") {
+        playerSelection == "PAPER" && computerSelection == "ROCK"
+    ) {
         result = "You win!";
         playerScore++;
     }
-    return result;
+    console.log(result)
 }
 
-console.log(playRound(playerSelection, computerSelection));
-console.log(playerScore);
-console.log(computerScore);
+function game() {
+    while (gameOn) {
+        playerSelection = getPlayerChoice();
+        computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log(`score player: ${playerScore}`);
+        console.log(`score computer: ${computerScore}`);
+        if (playerScore === 5) {
+            gameOn = false;
+            console.log("You won the BO5")
+        } else if (computerScore === 5) {
+            gameOn = false;
+            console.log("You lost the BO5")
+        }
+    }
+}
+
+game();
+
