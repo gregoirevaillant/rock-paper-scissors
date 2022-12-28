355bc371d523091bdb8d7e8431190bf6428644b2
Groune <98833627+gregoirevaillant@users.noreply.github.com>
2022-12-28 22:21:21 +0100

    hello

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-let playerScore = 0;
-let computerScore = 0;
+let playerScoreCount = 0;
+let computerScoreCount = 0;
 let gameOn = true;
 let playerSelection;
 let computerSelection;
+let playerChoice;
+
+let playerButtonRock = document.querySelector("#ROCK");
+let playerButtonPaper = document.querySelector("#PAPER");
+let playerButtonScissors = document.querySelector("#SCISSORS");
+
+let buttons = document.querySelectorAll(".playerChoice");
+
+let isChoiceMade = false;
+
+let playerScore = document.querySelector("#scorePlayer");
+let computerScore = document.querySelector("#scoreComputer");
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -17,48 +29,67 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    let choice = prompt("Rock, Paper, Scissors?");
-    choice = choice.toUpperCase();
-    return choice;
+
+    playerChoice = prompt("What is your choice? ");
+    return playerChoice;
+
 }
 
-function playRound(playerSelection, computerSelection) {
+function getWinnerRound(playerSelection, computerSelection) {
     let result;
     if (playerSelection == computerSelection) {
         result = "It's a tie!";
+        console.log(result)
     } else if (
         playerSelection == "ROCK" && computerSelection == "PAPER" ||
         playerSelection == "PAPER" && computerSelection == "SCISSORS" ||
         playerSelection == "SCISSORS" && computerSelection == "ROCK"
     ) {
         result = "You lose!";
-        computerScore++;
+        console.log(result)
+        computerScoreCount++;
     } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER" ||
         playerSelection == "ROCK" && computerSelection == "SCISSORS" ||
         playerSelection == "PAPER" && computerSelection == "ROCK"
     ) {
         result = "You win!";
-        playerScore++;
-    }
-    console.log(result)
-}
-
-function game() {
-    while (gameOn) {
-        playerSelection = getPlayerChoice();
-        computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log(`score player: ${playerScore}`);
-        console.log(`score computer: ${computerScore}`);
-        if (playerScore === 5) {
-            gameOn = false;
-            console.log("You won the BO5")
-        } else if (computerScore === 5) {
-            gameOn = false;
-            console.log("You lost the BO5")
-        }
+        console.log(result)
+        playerScoreCount++;
     }
 }
 
-game();
+function playRound() {
+    computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    playerSelection = getPlayerChoice();
+    console.log(playerSelection)
+
+    getWinnerRound(playerSelection, computerSelection);
+    playerScore.textContent = playerScoreCount;
+    computerScore.textContent = computerScoreCount;
+
+
+}
+
+playRound();
+
+
+// function game() {
+//     while (gameOn) {
+//         playerSelection = getPlayerChoice();
+//         computerSelection = getComputerChoice();
+//         playRound(playerSelection, computerSelection);
+//         console.log(`score player: ${playerScore}`);
+//         console.log(`score computer: ${computerScore}`);
+//         if (playerScore === 5) {
+//             gameOn = false;
+//             console.log("You won the BO5")
+//         } else if (computerScore === 5) {
+//             gameOn = false;
+//             console.log("You lost the BO5")
+//         }
+//     }
+// }
+
+// game();
 
