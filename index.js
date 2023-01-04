b1529c871338317bf1ac21f50a0e36bf349320d5
Grégoire VAILLANT <98833627+gregoirevaillant@users.noreply.github.com>
2023-01-04 23:11:50 +0100

    Clean the code and add comments

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// Add a looping icon to the tab
+//Get the element and count
 let tabImage = document.querySelector("link[rel='icon']");
 let imageCount = 0;
 
+// The function to change the image
 function changeImage() {
   imageCount++;
   if (imageCount === 3) imageCount = 0;
   tabImage.setAttribute("href", `./images/${imageCount}.ico`);
 }
 
+// Create the interval to change the image
 setInterval(() => {
   changeImage();
 }, 1000);
@@ -15,30 +19,24 @@ let playerScore = 0;
 let computerScore = 0;
 let roundWinner = "";
 
-let buttons = document.querySelectorAll(".buttonPlayer");
-let resetButton = document.querySelector("#RESET");
-
 let roundInformation = document.querySelector("#roundInfo");
 
 let computerScoreContent = document.querySelector("#scoreComputer");
 let playerScoreContent = document.querySelector("#scorePlayer");
 
-let playerSelection;
-let computerSelection;
-
-resetButton.addEventListener("click", () => {
-  resetGame();
-});
+// Check for an event on every buttons to launch the round
+let buttons = document.querySelectorAll(".buttonPlayer");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    playerSelection = button.id;
-    computerSelection = getComputerChoice();
+    let playerSelection = button.id;
+    let computerSelection = getComputerChoice();
     if (!isGameOver(playerScore, computerScore))
       playGame(playerSelection, computerSelection);
   });
 });
 
+// Generate the computer choice for the playround function
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   switch (choice) {
@@ -51,11 +49,10 @@ function getComputerChoice() {
   }
 }
 
+// Check the winner of the round, add to score
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     roundWinner = "tie";
-    console.log(playerSelection);
-    console.log(computerSelection);
   } else if (
     (playerSelection == "ROCK" && computerSelection == "PAPER") ||
     (playerSelection == "PAPER" && computerSelection == "SCISSORS") ||
@@ -63,8 +60,6 @@ function playRound(playerSelection, computerSelection) {
   ) {
     roundWinner = "computer";
     computerScore++;
-    console.log(playerSelection);
-    console.log(computerSelection);
   } else if (
     (playerSelection == "SCISSORS" && computerSelection == "PAPER") ||
     (playerSelection == "ROCK" && computerSelection == "SCISSORS") ||
@@ -72,11 +67,10 @@ function playRound(playerSelection, computerSelection) {
   ) {
     roundWinner = "player";
     playerScore++;
-    console.log(playerSelection);
-    console.log(computerSelection);
   }
 }
 
+// Update the score of the visually and for the isGameOver function
 function updateScore() {
   if (roundWinner === "tie") roundInformation.textContent = "Its a tie!";
   else if (roundWinner === "computer")
@@ -86,6 +80,7 @@ function updateScore() {
   playerScoreContent.textContent = playerScore;
 }
 
+// Display the final result when the game is over
 function finalResult() {
   if (playerScore === 5) {
     roundInformation.textContent = `You won the game with a score of ${playerScore}!`;
@@ -94,17 +89,28 @@ function finalResult() {
   }
 }
 
+// Play the game when a button is clicked
 function playGame(playerSelection, computerSelection) {
   playRound(playerSelection, computerSelection);
   updateScore();
   if (isGameOver(playerScore, computerScore)) finalResult();
 }
 
+// Check if computer or player get a score of 5
 function isGameOver(playerScore, computerScore) {
   if (playerScore === 5 || computerScore === 5) return true;
   else return false;
 }
 
+// Create a button to reset the game after the winner is announced
+let resetButton = document.querySelector("#RESET");
+
+// Listen for a click on the reset button and call restGame function
+resetButton.addEventListener("click", () => {
+  resetGame();
+});
+
+// Put all counter to 0 and text to neutral
 function resetGame() {
   computerScore = 0;
   playerScore = 0;
